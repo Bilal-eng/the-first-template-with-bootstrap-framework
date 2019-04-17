92a80993ff952360fe6b8a72a4baf26121f55f58
Bilal Sheikh Khalil <bilalsheikhkhalil@gmail.com>
2019-04-17 19:54:54 +0300

    add click action to every list item to change the color

--- js/plugins.js
+++ js/plugins.js
@@ -12,4 +12,17 @@ $(document).ready(function(){
         $('.color-option').toggle(200);
     });
 
+    // Change Theme Color On Click
+
+    var colorLi = $('.color-option ul li');
+
+    colorLi
+        .eq(0).css('backgroundColor', '#E41B17').end()
+        .eq(1).css('backgroundColor', '#E426D5').end()
+        .eq(2).css('backgroundColor', '#009AFF').end()
+        .eq(3).css('backgroundColor', '#FFF400').end()
+
+    colorLi.click(function(){
+        $('link[href*="theme"]').attr('href', $(this).attr('data-value'));
+    });
 });
