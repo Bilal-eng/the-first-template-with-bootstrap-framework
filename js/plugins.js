3f03850cf3b26638c77962b56c09f185edb86ced
Bilal Sheikh Khalil <bilalsheikhkhalil@gmail.com>
2019-04-17 19:22:21 +0300

    add option box to change the color

--- js/plugins.js
+++ js/plugins.js
@@ -2,7 +2,14 @@ $(document).ready(function(){
 
     $('.carousel').carousel({
 
-        interval: 5000
+        interval: 6000
+    });
+
+    // Show Color Option Div When Click On The Gear
+
+    $('.gear-check').click(function(){
+
+        $('.color-option').toggle(200);
     });
 
 });
